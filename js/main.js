7ae81e1e291ca6ce5092a6d73fe39874376d19e0
Alex-Uva89 <a.uva1988@gmail.com>
2022-02-28 15:17:08 +0100

    exc 2.1

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ let nameUser = prompt('Qual è il tuo nome?');
 let convalid = false;
 
 for (let controlList = 0; controlList < InvitedArray.length; controlList++) {
-    if (nameUser === InvitedArray[controlList]){
+    if (nameUser.toLowerCase === InvitedArray[controlList].toLowerCase){
         convalid = true;
     } 
 }
@@ -108,22 +108,74 @@ resultStamp.innerHTML= (calcArray);
 // Chiedi un numero di 4 cifre all’utente
 // e calcola la somma di tutte le cifre che compongono il numero.
 
-
-let numUser = parseInt(prompt('Indicami un numero di quattro cifre'));
+/*
+let numUser = (prompt('Indicami un numero di quattro cifre'));
 let numberArray = [];
+let summ = 0;
 
 if (numUser.length > 4){
     alert('Indica SOLO 4 cifre');
-    } else if (numUser.length < 4){
+} else if (numUser.length < 4){
     alert('Indica SOLO 4 cifre');
-    } else {
-        let summ = 0;
+} else {
         numberArray = [...numUser]
         for (let calc = 0; calc < numberArray.length; calc++) {
-             summ += parselInt(numberArray[calc]);
+             summ += parseInt(numUser[calc]);
         }
-        resultStamp.innerHTML = summ;
+}
+    
+    resultStamp.innerHTML = 'La somma dei 4 numeri è ' + summ;
+*/
+
+
+// :::::::::::::::SNACK 1.9
+/*
+Calcola la somma e la media dei primi 10 numeri.
+
+
+let somma = 0;
+
+for (let numero = 1; numero <= 10; numero++){
+    somma += numero;
+    console.log(numero);
 }
 
-    
+let media = somma / 10;
 
+console.log(somma);
+console.log(media);
+
+
+*/
+
+/*
+ Snack 2.1
+Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Esegui questo programma in due versioni, con il for e con il while.
+*/
+
+// chiedo 5 volte un numero
+// mi serve la somma dei numeri inseriti
+
+let somma = 0;
+
+for (let numero = 1; numero <= 5; numero++) {
+    let numeroUser = parseInt(prompt('Inserici un numero'));
+    somma += numeroUser; 
+}
+console.log(somma)
+
+
+
+/*
+Snack 2.2
+Inserisci un numero, se è pari stampa il numero,
+se è dispari stampa il numero successivo
+
+
+
+
+Snack 2.3
+Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+*/
